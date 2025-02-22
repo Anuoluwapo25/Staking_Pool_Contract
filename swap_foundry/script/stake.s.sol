06c21e10b42160174fb600d4fac47be82b65dcdd
Anuoluwapo Ali <125496062+Anuoluwapo25@users.noreply.github.com>
2025-02-22 18:01:22 +0100

    Update stake.s.sol

--- swap_foundry/script/stake.s.sol
+++ swap_foundry/script/stake.s.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
+import {Script, console} from "forge-std/Script.sol";
+import {StakingPool} from "../src/StakingPool.sol";
+
+contract StakingPoolScript is Script {
+    StakingPool public stakingPool;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        stakingPool = new StakingPool();
+
+        vm.stopBroadcast();
+    }
+}
